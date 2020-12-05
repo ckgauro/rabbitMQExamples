25ff323f6e61184251473f3026b488e9ca5c6dec
Chandra kishor Gauro <ckgauro@gmail.com>
2020-12-05 15:39:39 -0500

    RabbitMQ exchange Fanout exchange producer

---
+++ exchange/fanout/rabbitmq-fanout-exchange-consumer/src/main/java/com/gauro/rabbitmqfanoutexchangeconsumer/RabbitmqFanoutExchangeConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.gauro.rabbitmqfanoutexchangeconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqFanoutExchangeConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqFanoutExchangeConsumerApplication.class, args);
+	}
+
+}
---
+++ exchange/fanout/rabbitmq-fanout-exchange-consumer/src/test/java/com/gauro/rabbitmqfanoutexchangeconsumer/RabbitmqFanoutExchangeConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gauro.rabbitmqfanoutexchangeconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqFanoutExchangeConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
